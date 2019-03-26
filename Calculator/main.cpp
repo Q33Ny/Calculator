cf1abadb08a9bbbbcaa4d4dde4b4e1b825c1707f
zaraphp94205@gmail.com <zaraphp94205@gmail.com>
2019-03-26 09:32:45 +0600

    Added ban on divisions by zero. Added a cycle repeat calculations.

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -15,32 +15,44 @@ char get_sign() {
 }
 
 
-
 int main() {
-
+	char answer;
 	float first_number;
 	float second_number;
 	float result;
 	char sign;
+	bool flag;
 
 	std::cout << "Calculator v 1.0" << std::endl;
-	std::cout << "Input first number: ";
-	std::cin >> first_number;
-	sign = get_sign();
-	std::cout << "Input second number: ";
-	std::cin >> second_number;
+	do {
+		std::cout << "Input first number: ";
+		std::cin >> first_number;
+		sign = get_sign();
+
+		do {
+			std::cout << "Input second number: ";
+			std::cin >> second_number;
+			if ((sign == '/') && (second_number == 0)) {
+				std::cout << "It isn't impossible divided by zero !" << std::endl;
+				flag = false;
+			}
+			else
+				flag = true;
+		} while (flag != true);
 
 
-	if (sign == '+')
-		result = first_number + second_number;
-	else if (sign == '-')
-		result = first_number - second_number;
-	else if (sign == '*')
-		result = first_number * second_number;
-	else
-		result = first_number / second_number;
+		if (sign == '+')
+			result = first_number + second_number;
+		else if (sign == '-')
+			result = first_number - second_number;
+		else if (sign == '*')
+			result = first_number * second_number;
+		else
+			result = first_number / second_number;
 
-	std::cout << "Result of arithmetic operation is " << result << std::endl;
-
+		std::cout << "Result of arithmetic operation is " << result << std::endl;
+	    std:: cout << "Do you want to repeat: ";
+		std::cin >> answer;
+	} while (answer == 'y' || answer == 'Y');
 	return 0;
 }
